6d16bf7e8d0ea4973604ec7d2881fd77fca2a8a4
EDUARDO_JIREH_CORREA_GARCIA <a.21308051280344@cbtis128.edu.mx>
2025-03-12 19:48:22 +0000

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,92 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: 'Lista de Productos: 0344',
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ProductosScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Producto {
+  final int id_producto;
+  final String nom;
+  final double precio;
+
+  Producto(
+      {required this.id_producto, required this.nom, required this.precio});
+}
+
+class ProductosScreen extends StatelessWidget {
+  // Lista de mapas con datos de productos
+  final List<Map<String, dynamic>> productosData = [
+    {'id_producto': 1, 'nom': 'Producto 1', 'precio': 10.99},
+    {'id_producto': 2, 'nom': 'Producto 2', 'precio': 20.99},
+    {'id_producto': 3, 'nom': 'Producto 3', 'precio': 30.99},
+    {'id_producto': 4, 'nom': 'Producto 4', 'precio': 40.99},
+    {'id_producto': 5, 'nom': 'Producto 5', 'precio': 50.99},
+  ];
+
+  // Convertir la lista de mapas a una lista de objetos Producto
+  List<Producto> get productos {
+    return productosData.map((data) {
+      return Producto(
+        id_producto: data['id_producto'],
+        nom: data['nom'],
+        precio: data['precio'],
+      );
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Lista de Productos: Eduardo 0344'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(16),
+        itemCount: productos.length,
+        separatorBuilder: (context, index) =>
+            SizedBox(height: 16), // Separador entre contenedores
+        itemBuilder: (context, index) {
+          final producto = productos[index];
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.primaries[index % Colors.primaries.length]
+                  .withOpacity(0.2), // Color diferente
+              borderRadius: BorderRadius.circular(12), // Bordes redondeados
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 6,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('ID: ${producto.id_producto}',
+                    style: TextStyle(fontSize: 16)),
+                Text('Nombre: ${producto.nom}', style: TextStyle(fontSize: 16)),
+                Text('Precio: \$${producto.precio.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
